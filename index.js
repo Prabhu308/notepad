a484d19ccfc8e2b6eb7e83599d915059e5cde66a
Prabhu <prabu502@gmail.com>
2025-04-04 16:53:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-// app.js
-const express = require('express');
-const app = express();
-const port = 3000;
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-// Start the server only if this file is run directly
-if (require.main === module) {
-    app.listen(port, () => {
-        console.log(`App running at http://localhost:${port}`);
-    });
-}
-
-module.exports = app; // Export the app for testing
